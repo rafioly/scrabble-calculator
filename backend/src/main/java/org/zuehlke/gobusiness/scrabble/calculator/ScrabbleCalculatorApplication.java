e3cf4beb1a6b22623657c96067fc2c8ec3f5a8ea
Rubiantoro, Rafiano <rafiano.rubiantoro@zuhlke.com>
2025-09-03 17:34:11 +0800

    Removed unused spring cache dependency

--- backend/src/main/java/org/zuehlke/gobusiness/scrabble/calculator/ScrabbleCalculatorApplication.java
+++ backend/src/main/java/org/zuehlke/gobusiness/scrabble/calculator/ScrabbleCalculatorApplication.java
@@ -2,10 +2,8 @@ package org.zuehlke.gobusiness.scrabble.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class ScrabbleCalculatorApplication {
 
     public static void main(String[] args) {
